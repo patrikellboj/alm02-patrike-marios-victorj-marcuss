489b2276c582b690091a6a6d242ad27c41cdf192
mario <mariosss@libero.it>
2020-09-06 01:51:15 +0200

    traslator test 100%

--- src/main/java/com/example/almspringprova/Translator.java
+++ src/main/java/com/example/almspringprova/Translator.java
@@ -1,8 +1,8 @@
+
 package com.example.almspringprova;
 
 import org.springframework.stereotype.Component;
 
-import javax.swing.*;
 import java.io.FileInputStream;
 import java.util.Arrays;
 import java.util.List;
@@ -12,38 +12,49 @@ import java.util.Random;
 @Component
 public class Translator {
 
+    private String propertiesPath = "src\\main\\resources\\fortune.properties";
+
     public String selectMsgAccordingToRule(Person person){
         String selectedList = "";
-        if (person.getGender().equals("female"))
+        if (person.getGender().equalsIgnoreCase("female"))
             selectedList="female_msg";
         else
             selectedList="male_msg";
         int element = getRandomValue();
-        String result= getMsgFromProperties(selectedList, element);
+        String rightList= getPropertiesRightListAsString(selectedList);
+        String result = splitStringAndGetRightElement(rightList,element);
         return result;
     }
 
 
     public int getRandomValue() {
         Random random = new Random();
-        int randomValue = random.nextInt(10);
+        int randomValue = random.nextInt(5);
         return randomValue; //
     }
 
 
-    public String getMsgFromProperties(String selectedList, int element) {
+    public String getPropertiesRightListAsString(String selectedList) { // we have many list fortune we have to select the right one!
         Properties properties = new Properties();
 
         try {
-            properties.load(new FileInputStream("src\\main\\resources\\fortune.properties"));
+            properties.load(new FileInputStream(propertiesPath));
         }
         catch (Exception e){
-            System.out.println("File not found");
+            System.out.println("Properties file not found");
         }
-      //take the properties string (it's just a one long string), splitter it by "," and then get the right msg
-        String list = properties.getProperty(selectedList);
-        List<String> elements = Arrays.asList(list.split(","));
+        //take the properties string (it's just a one long string), splitter it by "," and then get the right msg
+        String rightList = properties.getProperty(selectedList);
+        return rightList;
+
+    }
+
+    public String splitStringAndGetRightElement (String rightList, int element) {
+        List<String> elements = Arrays.asList(rightList.split(","));
         return elements.get(element);
     }
 
+    public void setPropertiesPath(String propertiesPath) {
+        this.propertiesPath = propertiesPath;
+    }
 }
